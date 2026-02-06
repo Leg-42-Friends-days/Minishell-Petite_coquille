51ced5f071d0e83a8e7e8a15dfe66efbc4879074
mickzhan <mickzhan@student.42.fr>
2026-02-06 15:05:30 +0100

    correction free pwd et gitignore

--- minishell/src/pwd.c
+++ minishell/src/pwd.c
@@ -6,18 +6,32 @@
 /*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 14:31:27 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/02/06 14:53:21 by mickzhan         ###   ########.fr       */
+/*   Updated: 2026/02/06 15:04:36 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*ft_strcpy(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s2[i])
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
 
 t_pwd	*current_directory_path(t_pwd *pwd)
 {
 	char	cdw[1024];
 	int		i;
 
-	i = strlen(getcwd(cdw, sizeof((cdw))));
+	i = ft_strlen(getcwd(cdw, sizeof((cdw))));
 	if (i == 0)
 		perror("getwcd");
 	if (pwd->oldpwd && pwd->pwd)
@@ -28,7 +42,7 @@ t_pwd	*current_directory_path(t_pwd *pwd)
 	else
 	{
 		pwd->pwd = malloc(sizeof(char) * (i + 1));
-		strcpy(pwd->pwd, cdw);
+		pwd->pwd = ft_strcpy(pwd->pwd, cdw);
 		pwd->oldpwd = pwd->pwd;
 	}
 	return (pwd);
