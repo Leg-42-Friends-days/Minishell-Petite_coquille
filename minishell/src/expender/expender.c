e5b4b58c12cc1d9994eae5d5dcb37e012f3eb767
mickzhan <mickzhan@student.42.fr>
2026-03-07 15:39:06 +0100

    commentaire retirer pour norme

--- minishell/src/expender/expender.c
+++ minishell/src/expender/expender.c
@@ -6,7 +6,7 @@
 /*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 16:29:35 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/03/07 14:21:21 by mickzhan         ###   ########.fr       */
+/*   Updated: 2026/03/07 15:38:43 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -220,62 +220,6 @@ int	expand_len(t_ast *ast)
 	}
 	return (i);
 }
-
-// dans le return (free_split(tmp));
-
-// t_ast	*expension(t_ast *ast, t_sub_token *current_sub, t_env *env)
-// {
-// 	char	**tmp;
-// 	int		i;
-// 	int		j;
-
-// 	i = 0;
-// 	j = 0;
-// 	if (current_sub->quote == DOUBLE)
-// 	{
-// 		current_sub->var = app_expend(current_sub->var, env, true);
-// 		ast->cmd[i] = ft_strdup(current_sub->var);
-// 		i++;
-// 	}
-// 	else if (current_sub->quote == NORMAL)
-// 	{
-// 		current_sub->var = app_expend(current_sub->var, env, false);
-// 		tmp = ft_split(current_sub->var, ' ');
-// 		while (tmp[j])
-// 		{
-// 			ast->cmd[i] = ft_strdup(tmp[j]);
-// 			j++;
-// 			i++;
-// 		}
-// 	}
-// 	return (ast->cmd[i] = NULL, ast);
-// }
-
-// t_ast	*call_expand(t_ast *ast, t_env *env)
-// {
-// 	t_token		*current_token;
-// 	t_sub_token	*current_sub;
-// 	int			i;
-
-// 	current_token = ast->cmd_token;
-// 	while (current_token != NULL && current_token->type == WORD)
-// 	{
-// 		current_sub = current_token->sub_token;
-// 		while (current_sub != NULL)
-// 		{
-// 			ast = expension(ast, current_sub, env);
-// 			current_sub = current_sub->next;
-// 		}
-// 		current_token = current_token->next;
-// 	}
-// 	i = 0;
-// 	while (ast->cmd[i])
-// 	{
-// 		printf("PRINT CMD : %s\n", ast->cmd[i]);
-// 		i++;
-// 	}
-// 	return (ast);
-// }
 
 char	*strjoin_exp(char *s1, char *s2)
 {
