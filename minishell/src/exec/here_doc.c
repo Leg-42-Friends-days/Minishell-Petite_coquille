c9942c0a387c072cc2fb3376c0c9ef700a02af40
Iona Brouin-Benaissa <ibrouin-@f4r3s1.clusters.42paris.fr>
2026-04-01 23:46:22 +0200

    here_doc a merde

--- minishell/src/exec/here_doc.c
+++ minishell/src/exec/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: ibrouin- <ibrouin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 11:45:00 by ibrouin-          #+#    #+#             */
-/*   Updated: 2026/04/01 22:55:38 by ibrouin-         ###   ########.fr       */
+/*   Updated: 2026/04/01 23:46:07 by ibrouin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,16 @@ int	empty_line(char *line, int *fd)
 		close(fd[1]);
 		return(1);
 	}
-	return (0);
+	return (0);git add .
+}
+
+void	if_ctrl_c(int *fd, t_global *global)
+{
+	printf("rwtg\n");
+	close(fd[1]);
+	free_all_in_child(global, NULL);
+	g_signal = 130;
+	exit(130);
 }
 
 void	fill_here_doc(int *fd, t_redir **node, t_global *global)
@@ -59,13 +68,8 @@ void	fill_here_doc(int *fd, t_redir **node, t_global *global)
 		line = get_next_line(0);
 		if (empty_line(line, fd))
 			break ;
-		if (g_signal == 130)
-		{
-			close(fd[1]);
-			free_all_in_child(global, NULL);
-			g_signal = 0;
-			exit(1);			
-		}
+		//if (g_signal == 130)
+		//	if_ctrl_c(fd, global);
 		if (line[ft_strlen(line) - 1] == '\n')
 			line[ft_strlen(line) - 1] = '\0';
 		if ((ft_strncmp(line, (*node)->target->sub_token->var,
@@ -73,11 +77,8 @@ void	fill_here_doc(int *fd, t_redir **node, t_global *global)
 			if_limiter(line, global, fd);
 		if ((*node)->target->sub_token->quote == NONE)
 			line = app_expend(line, global, 0);
-		if (!line)
-		{
-			close(fd[1]);
+		if (empty_line(line, fd))
 			break ;
-		}
 		write(fd[1], line, ft_strlen(line));
 		write(fd[1], "\n", 1);
 		free(line);
@@ -87,7 +88,7 @@ void	fill_here_doc(int *fd, t_redir **node, t_global *global)
 void	child_here_doc(int *fd, t_redir *node, t_global *global)
 {
 	join_limiter(node);
-	g_signal = 0;
+	//g_signal = 0;
 	close(fd[0]);
 	signal(SIGINT, hand);
 	fill_here_doc(fd, &node, global);
@@ -101,8 +102,10 @@ int	prepare_here_doc(t_redir *node, t_global *global)
 	int		fd[2];
 	pid_t	pid;
 	int		status;
+	int		sig;
 
 	status = 0;
+	sig = 0;
 	init_child_signal_ig();
 	if (pipe(fd) == -1)
 		error_pipe();
@@ -120,6 +123,13 @@ int	prepare_here_doc(t_redir *node, t_global *global)
 	close_previous_heredocs(node);
 	node->fd = fd[0];
 	init_signals();
+	/* if (WIFSIGNALED(status))
+	{
+		sig = WTERMSIG(status);
+		printf("%d\n", sig);
+		return (signal_value(sig));
+	} */
+	printf("%d\n", g_signal);
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
 	return (0);
@@ -129,7 +139,9 @@ void	run_through_here_doc(t_ast *ast, t_env *env, t_global *global)
 {
 	t_ast	*current;
 	t_redir	*redir;
+	int		error;
 
+	error = 0;
 	current = ast;
 	if (ast != NULL)
 	{
@@ -141,8 +153,19 @@ void	run_through_here_doc(t_ast *ast, t_env *env, t_global *global)
 				while (redir)
 				{
 					if (redir->type == HEREDOC)
-						if (prepare_here_doc(redir, global) == 1)
+					{
+						error = prepare_here_doc(redir, global);
+						printf("%d\n", error);
+						if (error == 1)
 							global->here_doc_error = 1;
+						if (error == 256)
+						{
+							printf("mdrrrr\n");
+							global->here_doc_error = 1;
+							global->error_code = 130;
+							return ;
+						}
+					}
 					redir = redir->next;
 				}
 			}
